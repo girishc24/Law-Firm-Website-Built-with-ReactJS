400f6b91151f8aeafc47427bdf870f3b71591655
grishc24 <girishpluto@gmail.com>
2025-01-28 16:23:35 +0530

    Updated a Website

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Nav from "./Nav";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export {
+    Nav,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+}
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+
+
+export {
+    Hero,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    Footer,
+    PopularProduct,
+    SuperQuality
+}
